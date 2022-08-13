68e2e7b2cb7b8a28f6ee5af85bae5a99a7a4295b
loudoweb <loudoweb@gmail.com>
2022-08-13 20:29:52 +0200

    add 3 new blend modes

---
+++ openfl/shaders/blendMode/Multiply.hx
@@ -0,0 +1,48 @@
+package openfl.shaders.blendMode;
+
+import openfl.display.BitmapData;
+import openfl.display.GraphicsShader;
+import openfl.utils.ByteArray;
+
+/**
+ * Blend Mode
+ * @author Jamie Owen https://github.com/jamieowen/glsl-blend
+ * @author adapted by Loudo
+ */
+class Multiply extends GraphicsShader 
+{
+	
+	@:glFragmentSource(
+		"#pragma header
+		
+		uniform sampler2D foreground;
+		
+		
+		vec3 blendMultiply(vec3 base, vec3 blend) {
+			return base*blend;
+		}
+
+		vec3 blendMultiply(vec3 base, vec3 blend, float opacity) {
+			return (blendMultiply(base, blend) * opacity + base * (1.0 - opacity));
+		}
+		
+		void main(void) {
+			
+			vec4 bgColor = texture2D(bitmap, openfl_TextureCoordv);
+			vec4 fgColor = texture2D(foreground, openfl_TextureCoordv);
+			vec3 blendedColor = blendMultiply(bgColor.rgb, fgColor.rgb, fgColor.a);
+			gl_FragColor = vec4(blendedColor , bgColor.a);
+			gl_FragColor = gl_FragColor * openfl_Alphav;
+		}
+		
+		"
+	)
+
+	public function new(foreground:BitmapData) 
+	{
+		super();
+		data.foreground.input = foreground;
+		
+	}
+	
+}
---
+++ openfl/shaders/blendMode/Negation.hx
@@ -0,0 +1,49 @@
+package openfl.shaders.blendMode;
+
+import openfl.display.BitmapData;
+import openfl.display.GraphicsShader;
+import openfl.utils.ByteArray;
+
+/**
+ * Blend Mode
+ * @author Jamie Owen https://github.com/jamieowen/glsl-blend
+ * @author adapted by Loudo
+ * TODO add mask ?
+ */
+class Negation extends GraphicsShader 
+{
+	
+	@:glFragmentSource(
+		"#pragma header
+		
+		uniform sampler2D foreground;
+		
+		
+		vec3 blendNegation(vec3 base, vec3 blend) {
+			return vec3(1.0)-abs(vec3(1.0)-base-blend);
+		}
+
+		vec3 blendNegation(vec3 base, vec3 blend, float opacity) {
+			return (blendNegation(base, blend) * opacity + base * (1.0 - opacity));
+		}
+		
+		void main(void) {
+			
+			vec4 bgColor = texture2D(bitmap, openfl_TextureCoordv);
+			vec4 fgColor = texture2D(foreground, openfl_TextureCoordv);
+			vec3 blendedColor = blendNegation(bgColor.rgb, fgColor.rgb, fgColor.a);
+			gl_FragColor = vec4(blendedColor , bgColor.a);
+			gl_FragColor = gl_FragColor * openfl_Alphav;
+		}
+		
+		"
+	)
+
+	public function new(foreground:BitmapData) 
+	{
+		super();
+		data.foreground.input = foreground;
+		
+	}
+	
+}
